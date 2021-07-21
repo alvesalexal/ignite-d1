e8ae2d702071f85c6de716451def313a6b63f98d
Alex <alvesalexal@gmail.com>
2021-07-21 19:56:13 -0300

    v1

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { userName } = request.headers;
+  const user = users.find((user) => { user.userName === userName });
+
+  if (!user) {
+    return response.status(400).json({ message: "user not found" });
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const id = uuidv4();
+  const { userName } = request.body;
+  console.log(id + userName);
+
+  return response.status(201).json({ message: "user created" });
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
