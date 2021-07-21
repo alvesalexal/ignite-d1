85cf787c55d296c524defaf671ba0361378837f7
Alex <alvesalexal@gmail.com>
2021-07-21 19:56:25 -0300

    v1

--- src/index.js
+++ src/index.js
@@ -11,31 +11,50 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  const { userName } = request.headers;
-  const user = users.find((user) => { user.userName === userName });
-
-  if (!user) {
-    return response.status(400).json({ message: "user not found" });
+  const { userName } = request.body;
+  console.log(userName);
+  const user = users.find((user) => user.userName === userName );
+  if (user) {
+    return response.status(400).json({ message: "user already exists" });
   }
 
   request.user = user;
   return next();
 }
 
-app.post('/users', (request, response) => {
+app.post('/users', checksExistsUserAccount, (request, response) => {
   const id = uuidv4();
-  const { userName } = request.body;
-  console.log(id + userName);
+  const { userName, name } = request.body;
+  users.push({
+    name, userName, id,todo: []
+  });
 
-  return response.status(201).json({ message: "user created" });
+  return response.status(201).json(users);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request.headers;
+  console.log(username);
+  const user = users.find((user) => user.username === username );
+  if (user) {
+    return response.json(user.todo);
+  }
+  return response.status(400).json({ message: "user already not exists" });
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { username } = request.headers;
+  const user = users.find((user) => user.userName === username );
+  const id = uuidv4();
+  console.log(user)
+  if (user) {
+    user.todo.push({
+      title, deadline, done: false, id,todo
+    })
+    return response.status(201).json(user.todo);
+  }
+  return response.status(400).json({ message: "user already not exists" });
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
